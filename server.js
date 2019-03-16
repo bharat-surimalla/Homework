5562eada65ba12f4450363b6920168c87fe45687
bharat-surimalla <surimallabharat@gmail.com>
2019-03-15 19:47:45 -0500

    files uploaded

---
+++ server.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Movie = require("./data");
+
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+const cors = require('cors')
+
+// this is our MongoDB database
+const dbRoute = "mongodb+srv://user:mongo@clustermongo-vjjwv.mongodb.net/test?retryWrites=true";
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+app.use(cors())
+
+// this is our get method
+// this method fetches all available data in our database
+router.get("/getData", (req, res) => {
+	console.log("In Get");
+  Movie.find((err, data) => {
+	  console.log(data);
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+// this is our update method
+// this method overwrites existing data in our database
+router.post("/updateData", (req, res) => {
+  const { id, update } = req.body;
+  Movie.findOneAndUpdate(id, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// this is our delete method
+// this method removes existing data in our database
+router.delete("/deleteData", (req, res) => {
+  console.log(req);
+  const { id } = req.body;
+  Movie.findOneAndDelete(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+// this is our create methid
+// this method adds new data in our database
+router.post("/putData", (req, res) => {
+  let data = new Movie();
+
+  const { title, description } = req.body;
+
+  data.description = description;
+  data.title = title;
+  console.log(data);
+  data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// append /api for our http requests
+app.use("/api", router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => {console.log(`LISTENING ON PORT ${API_PORT}`);});
---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
+import { FaTrash } from 'react-icons/fa';
+
+
+class App extends Component {
+  // initialize our state 
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      id: 0,
+      message: null,
+      intervalIsSet: false,
+      idToDelete: null,
+      idToUpdate: null,
+      objectToUpdate: null
+      
+    };
+  }
+
+  // when component mounts, first thing it does is fetch all existing data in our db
+  // then we incorporate a polling logic so that we can easily see if our db has 
+  // changed and implement those changes into our UI
+  componentDidMount() {
+    this.getDataFromDb();
+    
+     if (!this.state.intervalIsSet) {
+       let interval = setInterval(this.getDataFromDb, 1000);
+        this.setState({ intervalIsSet: interval });
+     }
+  }
+
+  // never let a process live forever 
+  // always kill a process everytime we are done using it
+  componentWillUnmount() {
+    this.getDataFromDb();
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+
+  // our first get method that uses our backend api to 
+  // fetch data from our data base
+  getDataFromDb = () => {
+    fetch("http://localhost:3001/api/getData")
+      .then(data => data.json())
+      .then(res => {  this.setState({ data: res.data })});
+  };
+	
+	putDataToDB = (e) => {
+		//we don't want the form to submit, so we prevent the default behavior
+		e.preventDefault();
+		
+		var title = this.state.title.trim();
+		var description = this.state.description.trim();
+		axios.post("http://localhost:3001/api/putData", {title: title, description: description})
+			.then(function(response){console.log("post", response);});
+		
+		this.setState({
+		  title: '',
+		  description: ''
+    }); 	
+	  };
+
+
+  // our delete method that uses our backend api 
+  // to remove existing database information
+  deleteFromDB = (idTodelete) => {
+    axios.delete("http://localhost:3001/api/deleteData", {
+      data: {
+        id: idTodelete
+      }
+      
+    });
+  };
+
+
+  // here is our UI
+  // it is easy to understand their functions when you 
+  // see them render into our screen
+  render() {
+    const { data } = this.state;
+    return (
+        
+        <div class="container" >
+        <h1 class="text-center"> My Application</h1>
+      <form className="donationForm" onSubmit={this.putDataToDB}>
+        <div class="from-group">
+          <label for="pwd"> Title </label>
+          <input class="form-control"
+			  value={this.state.title}
+			  uniqueName="title"
+			  text="Title"
+			  onChange={e => this.setState({ title: e.target.value })} 
+			  />
+        </div>
+        <div class="from-group">
+        <label for="pwd">Description:</label>
+			<input
+        class="form-control"
+			  value={this.state.description}
+			  uniqueName="description"
+			  text="Description"
+			  onChange={e => this.setState({ description: e.target.value })} 
+			  />
+        </div>
+        <br></br>
+        <div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+
+      </form>
+
+            
+      <div >
+       <ol >  
+          {data.length <= 0
+            ? "NO DB ENTRIES YET"
+            : data.map(dat => (
+                <li style={{ padding: "10px" }} id={dat._id}>
+                  <span style={{ color: "gray" }}> Title: </span> {dat.title} <br />
+                  <span style={{ color: "gray" }}> Description: </span>
+                  {dat.description}
+                  {dat._id}  
+                
+                  <span style={{ margin : "0px 0px 0px 10px" }} class="btn btn-danger" 
+                  onClick={() => this.deleteFromDB(dat._id)}>
+                  <FaTrash />
+          </span>
+               
+                </li>
+              ))}
+        </ol>
+      </div>    
+    </div>
+        
+        
+    );
+  }
+}
+
+export default App;
